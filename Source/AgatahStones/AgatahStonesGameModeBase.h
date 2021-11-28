71dfbea1562d41c26b24b9732aa705ab12e490c2
adrienPeyrard <adrienPeyrard@users.noreply.github.com>
2021-11-28 17:57:04 +0100

    Initial commit

---
+++ Source/AgatahStones/AgatahStonesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AgatahStonesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGATAHSTONES_API AAgatahStonesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
